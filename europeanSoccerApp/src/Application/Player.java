259ac5bc85d71f8311d2e2ddd9e82c5ff7e27a04
Carlos Rodriguez <CarlosR@10.0.0.131>
2023-03-21 10:43:29 -0400

    Player class with getters, setters, equals, and toString

--- europeanSoccerApp/src/Application/Player.java
+++ europeanSoccerApp/src/Application/Player.java
@@ -33,7 +33,72 @@ public class Player {
 		this.currTeam = currTeam;
 		this.marketValue = marketValue;
 	}
+
+	//Getters
+	public String getName() {
+		return this.name;
+	}
+
+	public String getAge() {
+		return this.age;
+	}
+
+	public String getPosition() {
+		return this.position;
+	}
+
+	public String getNationalTeam() {
+		return this.nationalTeam;
+	}
+
+	public String getCurrTeam() {
+		return this.currTeam;
+	}
+
+	public double getMarketValue() {
+		return this.marketValue;
+	}
+
+	//Setters
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public void setNationalTeam(String nationalTeam) {
+		this.nationalTeam = nationalTeam;
+	}
+
+	public void setCurrTeam(String currTeam) {
+		this.currTeam = currTeam;
+	}
+
+	public void setMarketValue(double marketValue) {
+		this.marketValue = marketValue;
+	}
 	
+	//toString
+	public String toString() {
+		return String.format("Name: %s|Age: %s|Position: %s|National Team: %s|Current Team: %s|Market Value: %d", this.name, this.age,
+				this.position, this.nationalTeam, this.currTeam, this.marketValue);
+	}
+	
+	//equals
+	public boolean equals(Object obj) {
+		if(obj == null || !(obj instanceof Player)) {return false;}
+		if(obj == this) {return true;}
+		Player playerObj = (Player) obj;
+		return this.name.equals(playerObj.name) && this.age.equals(playerObj.age) && this.position.equals(playerObj.position)
+				&& this.nationalTeam.equals(playerObj.nationalTeam) && this.currTeam.equals(playerObj.currTeam) &&
+				this.marketValue == playerObj.marketValue;
+	}
 	
 	
 }
