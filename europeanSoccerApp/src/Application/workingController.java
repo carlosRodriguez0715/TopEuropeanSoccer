b6dbb9c781ed23792e576ecd22ce20dc6e0db20e
Carlos Rodriguez <CarlosR@10.0.0.131>
2023-03-24 13:39:41 -0400

    Initial development of workingSC fxml and its controller

---
+++ europeanSoccerApp/src/Application/workingController.java
@@ -0,0 +1,48 @@
+/**
+ * @author: Carlos Rodriguez
+ * Class handles the scene for the main application */
+
+package Application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class workingController implements Initializable{
+	
+	@FXML
+	private TextField searchBar;
+	
+	@FXML
+	private ChoiceBox<String> teamChoice, positionChoices, ageChoices;
+	
+	@FXML
+	private TextArea printArea;
+	
+	public void displayFound() {
+		
+	}
+	
+	public void showTChoices() {
+		
+	}
+	public void showAChoices() {
+	
+	}
+	
+	public void showPChoices() {
+		
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
