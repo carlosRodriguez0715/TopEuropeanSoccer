ff912720eb5fdf757614593152c93c7e9374060a
Carlos Rodriguez <CarlosR@10.0.0.131>
2023-03-28 17:41:27 -0400

    Removal of app and app controller. Main will start welcomesc. Few tests in main and working

--- europeanSoccerApp/src/Application/Main.java
+++ europeanSoccerApp/src/Application/Main.java
@@ -11,9 +11,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("App.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("WorkingSc.fxml"));
 			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
--- europeanSoccerApp/src/Application/workingController.java
+++ europeanSoccerApp/src/Application/workingController.java
@@ -5,17 +5,21 @@
 package Application;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class workingController extends Thread implements Initializable{
+public class workingController implements Initializable{
 	
 	private Queries q;
+	
+	private ArrayList<String> allTeams, allAges, allPositions;
 	
 	@FXML
 	private TextField searchBar;
@@ -28,6 +32,22 @@ public class workingController extends Thread implements Initializable{
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		this.printArea = new TextArea();
+		this.printArea.setText("oa");
+		this.q = new Queries();
+		this.allTeams = new ArrayList<String>();
+		this.allTeams.add("---");
+		this.allTeams.add("Liverpool");
+		this.allTeams.add("PSG");
+		this.allTeams.add("Barcelona");
+		this.teamChoice = new ChoiceBox<String>(FXCollections.observableArrayList(this.allTeams));
+		
+		this.allAges = new ArrayList<String>();
+		this.allAges.add("20");
+		this.allAges.add("21");
+		this.allAges.add("22");
+		this.allAges.add("23");
+		this.ageChoices = new ChoiceBox<String>(FXCollections.observableArrayList(this.allAges));
 	}
 	
 	public void displayFound() {
@@ -42,11 +62,6 @@ public class workingController extends Thread implements Initializable{
 	}
 	
 	public void showPChoices() {
-		
-	}
-	
-	@Override
-	public void run() {
 		
 	}
 	
